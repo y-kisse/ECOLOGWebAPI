789f70ba74e12b2c011a054111f49ff92a2322f4
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-07-09 17:02:51 +0900

    [add] Request Body Deserializer Model

---
+++ ECOLOGWebAPI/Models/CalcuratorRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOLOGWebAPI.Models
+{
+    /*
+     *  ECOLOG計算リクエストのリクエストボディデシリアライズクラス
+     */
+    public class CalcuratorRequest
+    {
+        public int Driver { get; set; }
+        public int Car { get; set; }
+        public int Sensor { get; set; }
+        IList<GPSTuple> GpsTuples { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+
+    public class GPSTuple
+    {
+        public DateTime GpsTime { get; set; }
+        public DateTime PhoneTime { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Altitude { get; set; }
+        public double Speed { get; set; }
+    }
+}
