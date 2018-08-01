c534c16eef6f315b117a9860db865a1de122eb52
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-08-01 13:58:10 +0900

    [add] EcologCalculator WIP

---
+++ ECOLOGWebAPI/Calculator/EcologCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECOLOGWebAPI.Models;
+
+namespace ECOLOGWebAPI.Calculator
+{
+    /*
+     * EcologCalculatorクラス
+     * SensorLogInserterReに存在するソースコードを用いて、EcologTupleを生成するクラス
+     * 基本的には2つのGPSデータからECOLOG計算を行い、その計算結果を返すようなものを想定する。
+     */
+    public class EcologCalculator
+    {
+        public static ECOLOGTuple calculateEcologTuple(GPSTuple previusGPS, GPSTuple currentGPSTuple)
+        {
+            // ECOLOGTupleの空のインスタンスを作成
+            ECOLOGTuple retTuple = new ECOLOGTuple();
+            
+            // GPSTupleから現在地、GPS時刻、速度を取得。
+            // 端末時刻は時刻同期がちゃんと取れてる必要があるので、GPS時刻を使うのが無難。
+            retTuple.Jst = currentGPSTuple.GpsTime;
+            retTuple.Latitude = currentGPSTuple.Latitude;
+            retTuple.Longitude = currentGPSTuple.Longitude;
+            retTuple.Speed = currentGPSTuple.Speed;
+
+            // 標高はMapMatching or 10mメッシュ標高データから取得する。
+
+            // 各種損失はSensorLogInserterReの計算メソッドを用いて算出する
+
+            // Efficiency
+
+            return retTuple;
+        }
+    }
+}
---
+++ ECOLOGWebAPI/Models/CalculatorResponse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ECOLOGWebAPI.Models
+{
+    /*
+     * 計算結果レスポンスシリアライズ用クラス
+     */
+    public class CalculatorResponse
+    {
+        public int Driver { get; set; }
+        public int Car { get; set; }
+        public int Sensor { get; set; }
+        public IList<ECOLOGTuple> EcologTuples { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("driver: {Driver}");
+            sb.AppendLine("car: {Car}");
+            sb.AppendLine("sensor: {Sensor}");
+            foreach(ECOLOGTuple tuple in EcologTuples)
+            {
+                sb.AppendLine(tuple.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+
+    /*
+     * 時間正規化した結果のシリアラライズクラス
+     */ 
+    public class ECOLOGTuple
+    {
+        public DateTime Jst { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Speed { get; set; }
+        public double TerrainAltitude { get; set; }
+        public double AirResistanceLoss { get; set; }
+        public double RollingLoss { get; set; }
+        public double ConvertLoss { get; set; }
+        public double RegeneLoss { get; set; }
+        public double LostEnergy { get; set; }
+        public double Efficiency { get; set; }
+        public string Link { get; set; }
+        public int SemanticLink { get; set; }
+    }
+}
