a2f12f81d0880edb2db094200d1407c4d3ce2e78
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-08-23 19:21:15 +0900

    [update] process to get altitude.

--- ECOLOGWebAPI/Calculator/EcologCalculator.cs
+++ ECOLOGWebAPI/Calculator/EcologCalculator.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Threading.Tasks;
 using ECOLOGWebAPI.Models;
+using SensorLogInserterRe.Calculators;
 
 namespace ECOLOGWebAPI.Calculator
 {
@@ -10,26 +12,32 @@ namespace ECOLOGWebAPI.Calculator
      * EcologCalculatorクラス
      * SensorLogInserterReに存在するソースコードを用いて、EcologTupleを生成するクラス
      * 基本的には2つのGPSデータからECOLOG計算を行い、その計算結果を返すようなものを想定する。
+     * 1レコード作成ごとにDataTable作成するのも大変そうなので、リクエスト時に近傍or指定の道路リンク集合を渡すことにした方が良さそう。  
      */
     public class EcologCalculator
     {
-        public static ECOLOGTuple calculateEcologTuple(GPSTuple previusGPS, GPSTuple currentGPSTuple)
+        public static ECOLOGTuple calculateEcologTuple(GPSTuple previusGPS, GPSTuple currentGPSTuple, DataTable roadLinks)
         {
             // ECOLOGTupleの空のインスタンスを作成
             ECOLOGTuple retTuple = new ECOLOGTuple();
             
             // GPSTupleから現在地、GPS時刻、速度を取得。
-            // 端末時刻は時刻同期がちゃんと取れてる必要があるので、GPS時刻を使うのが無難。
+            // 端末時刻は時刻同期がちゃんと取れてる必要があるので、GPS時刻を使うのが無難な気がする。
             retTuple.Jst = currentGPSTuple.GpsTime;
             retTuple.Latitude = currentGPSTuple.Latitude;
             retTuple.Longitude = currentGPSTuple.Longitude;
             retTuple.Speed = currentGPSTuple.Speed;
 
-            // 標高はMapMatching or 10mメッシュ標高データから取得する。
+            // 10mメッシュ標高データから取得する。
+            // シングルトンクラスのGetInstance()はインスタンスそのものを返すのでnewいらない。  
+            AltitudeCalculator altitudeCalculator = AltitudeCalculator.GetInstance();
+            Tuple<int, double> meshIdAltitudeTuple = altitudeCalculator.CalcAltitude(currentGPSTuple.Latitude, currentGPSTuple.Longitude);
+            retTuple.TerrainAltitude = meshIdAltitudeTuple.Item2;
 
             // 各種損失はSensorLogInserterReの計算メソッドを用いて算出する
 
             // Efficiency
+
 
             // link and semantic_link
             // 近傍リンクをSQLで指定、すべてのリンクに対して、GPS点とリンクの距離を求める
